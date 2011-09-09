4564d10e5f57557f0f72433a746d582ed3c3aae4
Thomas Rabaix <thomas.rabaix@ekino.com>
2011-09-09 20:38:36 +0200

    Update phpunit default configuration to work from within a standard install

--- Tests/tests/bootstrap.php
+++ Tests/tests/bootstrap.php
@@ -1,16 +1,15 @@
 <?php
 
 /*
- * This file is part of the Symfony package.
+ * this file is part of the symfony package.
  *
- * (c) Fabien Potencier <fabien.potencier@symfony-project.com>
+ * (c) fabien potencier <fabien.potencier@symfony-project.com>
  *
- * For the full copyright and license information, please view the LICENSE
+ * for the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
 
-if (file_exists($file = __DIR__.'/../autoload.php')) {
-    require_once $file;
-} elseif (file_exists($file = __DIR__.'/../autoload.php.dist')) {
-    require_once $file;
-}
+$rootDir = __DIR__.'/../../../../../../';
+
+require_once $rootDir.'/vendor/symfony/src/Symfony/Component/ClassLoader/UniversalClassLoader.php';
+require_once $rootDir.'/app/autoload.php';
