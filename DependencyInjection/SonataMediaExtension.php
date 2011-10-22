8014ece4966210123fdcdf13f8f10b95b4d50322
Thomas Rabaix <thomas.rabaix@ekino.com>
2011-10-22 12:27:48 +0200

    initial support for Admin Doctrine ODM support

--- DependencyInjection/SonataMediaExtension.php
+++ DependencyInjection/SonataMediaExtension.php
@@ -48,7 +48,7 @@ class SonataMediaExtension extends Extension
             $loader->load('formatter.xml');
         }
 
-        if (!in_array(strtolower($config['db_driver']), array('orm', 'mongodb'))) {
+        if (!in_array(strtolower($config['db_driver']), array('orm', 'doctrine_mongodb'))) {
             throw new \InvalidArgumentException(sprintf('Invalid db driver "%s".', $config['db_driver']));
         }
 
