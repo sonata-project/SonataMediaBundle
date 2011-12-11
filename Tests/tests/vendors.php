a881bb5d57860b6b3ec78bc16e61c9da32db3218
Thomas Rabaix <thomas.rabaix@ekino.com>
2011-12-11 16:15:55 +0100

    Fix KnpLabs namespace

--- Tests/tests/vendors.php
+++ Tests/tests/vendors.php
@@ -10,7 +10,7 @@ if (!is_dir($vendorDir)) {
 
 $deps = array(
     array('symfony', 'git://github.com/symfony/symfony.git', isset($_SERVER['SYMFONY_VERSION']) ? $_SERVER['SYMFONY_VERSION'] : 'origin/master'),
-    array('knpmenu', 'git://github.com/knplabs/KnpMenu.git', 'origin/master'),
+    array('knpmenu', 'git://github.com/KnpLabs/KnpMenu.git', 'origin/master'),
     array('Sonata/AdminBundle', 'git://github.com/sonata-project/SonataAdminBundle.git', 'origin/master'),
     array('gaufrette', 'git://github.com/KnpLabs/Gaufrette.git', 'origin/master')
 );
