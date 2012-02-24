b1930fe304094dc3a754de7237cd00d5d6797ddf
Thomas Rabaix <thomas.rabaix@ekino.com>
2012-02-24 12:54:46 +0100

    Add missing licence header

--- Listener/BaseMediaEventSubscriber.php
+++ Listener/BaseMediaEventSubscriber.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Sonata project.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Sonata\MediaBundle\Listener;
 
 use Doctrine\Common\EventSubscriber;
