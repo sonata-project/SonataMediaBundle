eb48e3399835cc5ee8dbff0dd72d1bc11b8f5b85
Rémi Marseille <marseille@ekino.com>
2012-03-29 15:09:19 +0200

    Fix pptx extension

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -189,7 +189,7 @@ class Configuration implements ConfigurationInterface
                                 ->scalarNode('thumbnail')->defaultValue('sonata.media.thumbnail.format')->end()
                                 ->scalarNode('allowed_extensions')->defaultValue(array(
                                     'pdf', 'txt', 'rtf',
-                                    'doc', 'docx', 'xls', 'xlsx', 'ppt', 'pttx',
+                                    'doc', 'docx', 'xls', 'xlsx', 'ppt', 'pptx',
                                     'odt', 'odg', 'odp', 'ods', 'odc', 'odf', 'odb',
                                     'csv',
                                     'xml',
