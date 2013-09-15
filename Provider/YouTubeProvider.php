c999c4e894942da90fbc5814f4a08f9d01e0a9a5
jpi <jl.pirson@grizzlylab.be>
2013-09-15 11:43:02 +0200

    YouTube video is now HTML5 compliant (replace .swf behavior).

--- Provider/YouTubeProvider.php
+++ Provider/YouTubeProvider.php
@@ -15,91 +15,85 @@ namespace Sonata\MediaBundle\Provider;
 use Sonata\MediaBundle\Model\MediaInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 
-class YouTubeProvider extends BaseVideoProvider
-{
+class YouTubeProvider extends BaseVideoProvider {
+
     /**
      * {@inheritdoc}
      */
-    public function getHelperProperties(MediaInterface $media, $format, $options = array())
-    {
+    public function getHelperProperties(MediaInterface $media, $format, $options = array()) {
         // documentation : http://code.google.com/apis/youtube/player_parameters.html
 
-        $defaults = array(
+        $default_player_parameters = array(
             //Values: 0 or 1. Default is 1. Sets whether the player should load related
             // videos once playback of the initial video starts. Related videos are
             // displayed in the "genie menu" when the menu button is pressed. The player
             // search functionality will be disabled if rel is set to 0.
-            'rel'            => 0,
-
+            'rel' => 0,
             // Values: 0 or 1. Default is 0. Sets whether or not the initial video will autoplay
             // when the player loads.
-            'autoplay'       => 0,
-
+            'autoplay' => 0,
             // Values: 0 or 1. Default is 0. In the case of a single video player, a setting of 1
             // will cause the player to play the initial video again and again. In the case of a
             // playlist player (or custom player), the player will play the entire playlist and
             // then start again at the first video.
-            'loop'           => 0,
-
+            'loop' => 0,
             // Values: 0 or 1. Default is 0. Setting this to 1 will enable the Javascript API.
             // For more information on the Javascript API and how to use it, see the JavaScript
             // API documentation.
-            'enablejsapi'    => 0,
-
+            'enablejsapi' => 0,
             // Values: 0 or 1. Default is 0. Setting to 1 enables the "Enhanced Genie Menu". This
             // behavior causes the genie menu (if present) to appear when the user's mouse enters
             // the video display area, as opposed to only appearing when the menu button is pressed.
-            'egm'            => 0,
-
-            // Values: 0 or 1. Default is 0. Setting to 1 enables a border around the entire video
-            // player. The border's primary color can be set via the color1 parameter, and a
-            // secondary color can be set by the color2 parameter.
-            'border'         => 0,
-
+            'egm' => 0,
             // Values: Any RGB value in hexadecimal format. color1 is the primary border color, and
             // color2 is the video control bar background color and secondary border color.
-            'color1'         => null,
-            'color2'         => null,
-            
-            // Values: 0 or 1. Default is 1. Setting to 0 disables the fullscreen button.
-            'allowfullscreen' => 1,
-
+            'color1' => null,
+            'color2' => null,
             // Values: A positive integer. This parameter causes the player to begin playing the video
             // at the given number of seconds from the start of the video. Note that similar to the
             // seekTo function, the player will look for the closest keyframe to the time you specify.
             // This means sometimes the play head may seek to just before the requested time, usually
             // no more than ~2 seconds
-            'start'          => 0,
-
+            'start' => 0,
             // Values: 0 or 1. Default is 0. Setting to 1 enables HD playback by default. This has no
             // effect on the Chromeless Player. This also has no effect if an HD version of the video
             // is not available. If you enable this option, keep in mind that users with a slower
             // connection may have an sub-optimal experience unless they turn off HD. You should ensure
             // your player is large enough to display the video in its native resolution.
-            'hd'             => 1,
-
+            'hd' => 1,
             // Values: 0 or 1. Default is 1. Setting to 0 disables the search box from displaying when
             // the video is minimized. Note that if the rel parameter is set to 0 then the search box
             // will also be disabled, regardless of the value of showsearch.
-            'showsearch'     => 0,
-
+            'showsearch' => 0,
             // Values: 0 or 1. Default is 1. Setting to 0 causes the player to not display information
             // like the video title and rating before the video starts playing.
-            'showinfo'       => 0,
-
+            'showinfo' => 0,
             // Values: 1 or 3. Default is 1. Setting to 1 will cause video annotations to be shown by
             // default, whereas setting to 3 will cause video annotation to not be shown by default.
             'iv_load_policy' => 1
         );
 
-        $player_parameters = array_merge($defaults, isset($options['player_parameters']) ? $options['player_parameters'] : array());
+        $default_player_attributes = array(
+            // Values: 0 or 1. Default is 0. Setting to 1 enables a border around the entire video
+            // player. The border's primary color can be set via the color1 parameter, and a
+            // secondary color can be set by the color2 parameter.
+            'border' => 0,
+            // Values: 0 or 1. Default is 1. Setting to 0 disables the fullscreen button.
+            'allowfullscreen' => 'allowfullscreen'
+        );
+
+        $player_parameters = array_merge($default_player_parameters, isset($options['player_parameters']) ? $options['player_parameters'] : array());
 
         $box = $this->getBoxHelperProperties($media, $format, $options);
 
+        $player_attributes = array_merge($default_player_attributes, isset($options['player_attributes']) ? $options['player_attributes'] : array(), array(
+            'width' => $box->getWidth(),
+            'height' => $box->getHeight()
+        ));
+
         $params = array(
             'player_parameters' => http_build_query($player_parameters),
-            'width'             => $box->getWidth(),
-            'height'            => $box->getHeight(),
+            'player_attributes' => $player_attributes
         );
 
         return $params;
@@ -108,8 +102,7 @@ class YouTubeProvider extends BaseVideoProvider
     /**
      * {@inheritdoc}
      */
-    protected function fixBinaryContent(MediaInterface $media)
-    {
+    protected function fixBinaryContent(MediaInterface $media) {
         if (!$media->getBinaryContent()) {
             return;
         }
@@ -122,8 +115,7 @@ class YouTubeProvider extends BaseVideoProvider
     /**
      * {@inheritdoc}
      */
-    protected function doTransform(MediaInterface $media)
-    {
+    protected function doTransform(MediaInterface $media) {
         $this->fixBinaryContent($media);
 
         if (!$media->getBinaryContent()) {
@@ -140,8 +132,7 @@ class YouTubeProvider extends BaseVideoProvider
     /**
      * {@inheritdoc}
      */
-    public function updateMetadata(MediaInterface $media, $force = false)
-    {
+    public function updateMetadata(MediaInterface $media, $force = false) {
         $url = sprintf('http://www.youtube.com/oembed?url=http://www.youtube.com/watch?v=%s&format=json', $media->getProviderReference());
 
         try {
@@ -168,8 +159,8 @@ class YouTubeProvider extends BaseVideoProvider
     /**
      * {@inheritdoc}
      */
-    public function getDownloadResponse(MediaInterface $media, $format, $mode, array $headers = array())
-    {
+    public function getDownloadResponse(MediaInterface $media, $format, $mode, array $headers = array()) {
         return new RedirectResponse(sprintf('http://www.youtube.com/watch?v=%s', $media->getProviderReference()), 302, $headers);
     }
+
 }
