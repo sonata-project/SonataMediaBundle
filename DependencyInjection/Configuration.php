d75bd045cbcc8eebf57388868b37cec00df65681
Ilya Pleshakov <da-eto@ya.ru>
2013-10-28 10:48:50 +0400

    Add "extra" configuration field in "formats" section (for additional resizer parameters)

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -81,6 +81,7 @@ class Configuration implements ConfigurationInterface
                                         ->scalarNode('quality')->defaultValue(80)->end()
                                         ->scalarNode('format')->defaultValue('jpg')->end()
                                         ->scalarNode('constraint')->defaultValue(true)->end()
+                                        ->variableNode('extra')->end()
                                     ->end()
                                 ->end()
                             ->end()
