590e4158b205ec596082f687c27407e9862ac528
Alexandru Benzari <benzari.alex@gmail.com>
2014-02-26 10:38:33 +0200

    fix SimpleResizer image ratios

--- Resizer/SimpleResizer.php
+++ Resizer/SimpleResizer.php
@@ -13,9 +13,11 @@ namespace Sonata\MediaBundle\Resizer;
 
 use Imagine\Image\ImagineInterface;
 use Imagine\Image\Box;
+use Imagine\Image\BoxInterface;
+use Imagine\Image\ImageInterface;
+use Imagine\Image\Point;
 use Gaufrette\File;
 use Sonata\MediaBundle\Model\MediaInterface;
-use Imagine\Image\ImageInterface;
 use Imagine\Exception\InvalidArgumentException;
 use Sonata\MediaBundle\Metadata\MetadataBuilderInterface;
 
@@ -30,6 +32,7 @@ class SimpleResizer implements ResizerInterface
     /**
      * @param ImagineInterface $adapter
      * @param string           $mode
+     * @param MetadataBuilderInterface $metadata
      */
     public function __construct(ImagineInterface $adapter, $mode, MetadataBuilderInterface $metadata)
     {
@@ -49,8 +52,8 @@ class SimpleResizer implements ResizerInterface
 
         $image = $this->adapter->load($in->getContent());
 
-        $content = $image
-            ->thumbnail($this->getBox($media, $settings), $this->mode)
+        $content = $this
+            ->thumbnail($image, $this->getBox($media, $settings), $this->mode, $this->computeRatio($media, $settings))
             ->get($format, array('quality' => $settings['quality']));
 
         $out->setContent($content, $this->metadata->get($media, $out->getName()));
@@ -63,36 +66,37 @@ class SimpleResizer implements ResizerInterface
     {
         $size = $media->getBox();
 
-        if ($settings['width'] == null && $settings['height'] == null) {
-            throw new \RuntimeException(sprintf('Width/Height parameter is missing in context "%s" for provider "%s". Please add at least one parameter.', $media->getContext(), $media->getProviderName()));
-        }
-
-        if ($settings['height'] == null) {
-            $settings['height'] = (int) ($settings['width'] * $size->getHeight() / $size->getWidth());
-        }
-
-        if ($settings['width'] == null) {
-            $settings['width'] = (int) ($settings['height'] * $size->getWidth() / $size->getHeight());
-        }
-
-        return $this->computeBox($media, $settings);
+        return $size->scale($this->computeRatio($media, $settings));
     }
 
     /**
      * @throws InvalidArgumentException
+     * @throws \RuntimeException
      *
      * @param MediaInterface $media
      * @param array          $settings
      *
-     * @return Box
+     * @return float         $ratio
      */
-    private function computeBox(MediaInterface $media, array $settings)
+    private function computeRatio(MediaInterface $media, array $settings)
     {
         if ($this->mode !== ImageInterface::THUMBNAIL_INSET && $this->mode !== ImageInterface::THUMBNAIL_OUTBOUND) {
             throw new InvalidArgumentException('Invalid mode specified');
         }
 
         $size = $media->getBox();
+
+        if ($settings['width'] == null && $settings['height'] == null) {
+            throw new \RuntimeException(sprintf('Width/Height parameter is missing in context "%s" for provider "%s". Please add at least one parameter.', $media->getContext(), $media->getProviderName()));
+        }
+
+        if ($settings['height'] == null) {
+            $settings['height'] = $settings['width'] * $size->getHeight() / $size->getWidth();
+        }
+
+        if ($settings['width'] == null) {
+            $settings['width'] = $settings['height'] * $size->getWidth() / $size->getHeight();
+        }
 
         $ratios = array(
             $settings['width'] / $size->getWidth(),
@@ -105,6 +109,54 @@ class SimpleResizer implements ResizerInterface
             $ratio = max($ratios);
         }
 
-        return $size->scale($ratio);
+        return $ratio;
+    }
+
+
+    /**
+     * @param ImageInterface $image
+     * @param BoxInterface $size
+     * @param string $mode
+     * @param float $ratio
+     *
+     * @return \Imagine\Image\ManipulatorInterface
+     */
+    private function thumbnail(ImageInterface $image, BoxInterface $size, $mode = ImageInterface::THUMBNAIL_INSET, $ratio)
+    {
+
+        $imageSize = $image->getSize();
+        $thumbnail = $image->copy();
+
+        // if target width is larger than image width
+        // AND target height is longer than image height
+        if ($size->contains($imageSize)) {
+            return $thumbnail;
+        }
+
+        if ($mode === ImageInterface::THUMBNAIL_OUTBOUND) {
+            if (!$imageSize->contains($size)) {
+                $size = new Box(
+                    min($imageSize->getWidth(), $size->getWidth()),
+                    min($imageSize->getHeight(), $size->getHeight())
+                );
+            } else {
+                $imageSize = $thumbnail->getSize()->scale($ratio);
+                $thumbnail->resize($imageSize);
+            }
+            $thumbnail->crop(new Point(
+                max(0, round(($imageSize->getWidth() - $size->getWidth()) / 2)),
+                max(0, round(($imageSize->getHeight() - $size->getHeight()) / 2))
+            ), $size);
+        } else {
+            if (!$imageSize->contains($size)) {
+                $imageSize = $imageSize->scale($ratio);
+                $thumbnail->resize($imageSize);
+            } else {
+                $imageSize = $thumbnail->getSize()->scale($ratio);
+                $thumbnail->resize($imageSize);
+            }
+        }
+
+        return $thumbnail;
     }
 }
