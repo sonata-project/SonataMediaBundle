d956c5e2e312968f4c58ad1d0c41f69302c957c4
Florent Denis <fdenis@ekino.com>
2014-06-06 16:50:14 +0200

    Update manager service

--- PHPCR/GalleryManager.php
+++ PHPCR/GalleryManager.php
@@ -8,67 +8,24 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+ 
 namespace Sonata\MediaBundle\PHPCR;
 
+use Sonata\CoreBundle\Model\BaseDocumentManager;
 use Sonata\MediaBundle\Model\GalleryManager as AbstractGalleryManager;
 use Sonata\MediaBundle\Model\GalleryInterface;
-use Sonata\DoctrinePHPCRAdminBundle\Model\ModelManager;
-use Sonata\MediaBundle\PHPCR\MediaManager;
 
-class GalleryManager extends AbstractGalleryManager
+class GalleryManager extends BaseDocumentManager
 {
-    protected $modelManager;
-    protected $class;
-    protected $mediaManager;
-
     /**
-     * @param \Sonata\DoctrinePHPCRAdminBundle\Model\ModelManager $modelManager
-     * @param $class
-     */
-    public function __construct(ModelManager $modelManager, $class, MediaManager $mediaManager)
-    {
-        $this->modelManager = $modelManager;
-        $this->class        = $class;
-        $this->mediaManager = $mediaManager;
-    }
-
-    /**
-     * {@inheritdoc}
+     * BC Compatibility.
+     *
+     * @deprecated Please use save() from now
+     *
+     * @param GalleryInterface $gallery
      */
     public function update(GalleryInterface $gallery)
     {
-        $this->modelManager->update($gallery);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getClass()
-    {
-        return $this->class;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function findOneBy(array $criteria)
-    {
-        return $this->mediaManager->findOneBy($criteria);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function findBy(array $criteria)
-    {
-        return $this->mediaManager->findBy($criteria);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function delete(GalleryInterface $gallery)
-    {
-        $this->modelManager->delete($gallery);
+        parent::save($gallery);
     }
 }
