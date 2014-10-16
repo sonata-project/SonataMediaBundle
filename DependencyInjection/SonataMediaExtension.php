ff199ffb6a80fe293d0bf792cd4acb99ca94301e
Hugo Briand <briand@ekino.com>
2014-10-16 17:48:29 +0200

    Upgraded Amazon S3 SDK

--- DependencyInjection/SonataMediaExtension.php
+++ DependencyInjection/SonataMediaExtension.php
@@ -363,8 +363,7 @@ class SonataMediaExtension extends Extension
             $container->getDefinition('sonata.media.adapter.filesystem.s3')
                 ->replaceArgument(0, new Reference('sonata.media.adapter.service.s3'))
                 ->replaceArgument(1, $config['filesystem']['s3']['bucket'])
-                ->replaceArgument(2, array('create' => $config['filesystem']['s3']['create'], 'region' => $config['filesystem']['s3']['region']))
-                ->addMethodCall('setDirectory', array($config['filesystem']['s3']['directory']));
+                ->replaceArgument(2, array('create' => $config['filesystem']['s3']['create'], 'region' => $config['filesystem']['s3']['region'], 'directory' => $config['filesystem']['s3']['directory']))
             ;
 
             $container->getDefinition('sonata.media.metadata.amazon')
