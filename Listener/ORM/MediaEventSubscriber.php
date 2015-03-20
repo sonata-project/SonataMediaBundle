9e187c66b03eeb885cddef861a1e47c61cc538ac
George Bardis <george@bardis.info>
2015-03-20 13:26:37 +0000

    request the sonata classification root categories mapped by context name instead of the default, that is id, as the context for each media item is set by name and this causes incompatibility between the two bundles

--- Listener/ORM/MediaEventSubscriber.php
+++ Listener/ORM/MediaEventSubscriber.php
@@ -75,7 +75,7 @@ class MediaEventSubscriber extends BaseMediaEventSubscriber
     protected function getRootCategory(MediaInterface $media)
     {
         if (!$this->rootCategories) {
-            $this->rootCategories = $this->container->get('sonata.classification.manager.category')->getRootCategories(false);
+            $this->rootCategories = $this->container->get('sonata.classification.manager.category')->getRootCategories(false, 'contextName');
         }
 
         if (!array_key_exists($media->getContext(), $this->rootCategories)) {
