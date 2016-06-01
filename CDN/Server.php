07b315a253199cf52b54593d83870da560ffef3b
Bozhidar Hristov <warxcell@gmail.com>
2016-06-01 22:44:42 +0300

    Update Server.php
    fix styling accoding StyleCI

--- CDN/Server.php
+++ CDN/Server.php
@@ -8,7 +8,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
- 
+
 namespace Sonata\MediaBundle\CDN;
 
 use Symfony\Bundle\FrameworkBundle\Templating\Helper\AssetsHelper;
