70c907375e92a10a4d52f015ab66b30a4afc550b
SonataCI <thomas+ci@sonata-project.org>
2017-11-11 02:07:22 +0100

    DevKit updates

--- .travis/before_install_test.sh
+++ .travis/before_install_test.sh
@@ -1,21 +1,19 @@
 #!/usr/bin/env sh
 set -ev
 
-if [ "${TRAVIS_PHP_VERSION}" != "hhvm" ]; then
-    PHP_INI_DIR="$HOME/.phpenv/versions/$(phpenv version-name)/etc/conf.d/"
-    TRAVIS_INI_FILE="$PHP_INI_DIR/travis.ini"
-    echo "memory_limit=3072M" >> "$TRAVIS_INI_FILE"
+PHP_INI_DIR="$HOME/.phpenv/versions/$(phpenv version-name)/etc/conf.d/"
+TRAVIS_INI_FILE="$PHP_INI_DIR/travis.ini"
+echo "memory_limit=3072M" >> "$TRAVIS_INI_FILE"
 
-    
-        if [ "$TRAVIS_PHP_VERSION" '<' '7.0' ]; then
-        echo "extension=mongo.so" >> "$TRAVIS_INI_FILE"
-    else
-        echo "extension=mongodb.so" >> "$TRAVIS_INI_FILE"
 
-        # Backwards compatibility with old mongo extension
-        composer require "alcaeus/mongo-php-adapter" --no-update
-    fi
-    fi
+if [ "$TRAVIS_PHP_VERSION" '<' '7.0' ]; then
+    echo "extension=mongo.so" >> "$TRAVIS_INI_FILE"
+else
+    echo "extension=mongodb.so" >> "$TRAVIS_INI_FILE"
+
+    # Backwards compatibility with old mongo extension
+    composer require "alcaeus/mongo-php-adapter" --no-update
+fi
 
 sed --in-place "s/\"dev-master\":/\"dev-${TRAVIS_COMMIT}\":/" composer.json
 
