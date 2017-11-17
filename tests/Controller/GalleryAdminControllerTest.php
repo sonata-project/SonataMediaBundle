47d5ef9486ba06b099dc40c97601d0aaa5632a22
Christian Kraus <github@hanzi.cc>
2017-11-17 18:06:05 +0700

    Use FormRenderer runtime to maintain compatibility with Symfony 3.4 (#1325)
    * Use FormRenderer runtime to maintain compatibility with Symfony 3.4
    
    * Update setFormTheme() code to match the corresponding method in AdminBundle
    
    * Use ClassName::class notation in test methods

--- tests/Controller/GalleryAdminControllerTest.php
+++ tests/Controller/GalleryAdminControllerTest.php
@@ -14,6 +14,11 @@ namespace Sonata\MediaBundle\Tests\Controller;
 use PHPUnit\Framework\TestCase;
 use Prophecy\Argument;
 use Sonata\MediaBundle\Controller\GalleryAdminController;
+use Symfony\Bridge\Twig\AppVariable;
+use Symfony\Bridge\Twig\Command\DebugCommand;
+use Symfony\Bridge\Twig\Extension\FormExtension;
+use Symfony\Bridge\Twig\Form\TwigRenderer;
+use Symfony\Component\Form\FormRenderer;
 
 class GalleryAdminControllerTest extends TestCase
 {
@@ -120,19 +125,27 @@ class GalleryAdminControllerTest extends TestCase
 
     private function configureSetFormTheme($formView, $formTheme)
     {
-        $twig = $this->prophesize('\Twig_Environment');
-        $twigRenderer = $this->prophesize('Symfony\Bridge\Twig\Form\TwigRenderer');
+        $twig = $this->prophesize(\Twig_Environment::class);
+
+        // Remove this trick when bumping Symfony requirement to 3.4+
+        if (method_exists(DebugCommand::class, 'getLoaderPaths')) {
+            $rendererClass = FormRenderer::class;
+        } else {
+            $rendererClass = TwigRenderer::class;
+        }
+
+        $twigRenderer = $this->prophesize($rendererClass);
 
         $this->container->get('twig')->willReturn($twig->reveal());
 
         // Remove this trick when bumping Symfony requirement to 3.2+.
-        if (method_exists('Symfony\Bridge\Twig\AppVariable', 'getToken')) {
-            $twig->getRuntime('Symfony\Bridge\Twig\Form\TwigRenderer')->willReturn($twigRenderer->reveal());
+        if (method_exists(AppVariable::class, 'getToken')) {
+            $twig->getRuntime($rendererClass)->willReturn($twigRenderer->reveal());
         } else {
-            $formExtension = $this->prophesize('Symfony\Bridge\Twig\Extension\FormExtension');
+            $formExtension = $this->prophesize(FormExtension::class);
             $formExtension->renderer = $twigRenderer->reveal();
 
-            $twig->getExtension('Symfony\Bridge\Twig\Extension\FormExtension')->willReturn($formExtension->reveal());
+            $twig->getExtension(FormExtension::class)->willReturn($formExtension->reveal());
         }
         $twigRenderer->setTheme($formView, $formTheme)->shouldBeCalled();
     }
