fc2e3381a63cf113d93b2513f32381e4c7134ba9
enekochan <enekochan@gmail.com>
2018-05-23 12:20:20 +0200

    Stop using Sonata\AdminBundle\Controller\CRUDController::render and use renderWithExtraParams

--- src/Controller/GalleryAdminController.php
+++ src/Controller/GalleryAdminController.php
@@ -35,7 +35,7 @@ class GalleryAdminController extends Controller
         $parameters['media_pool'] = $this->get('sonata.media.pool');
         $parameters['persistent_parameters'] = $this->admin->getPersistentParameters();
 
-        return parent::render($view, $parameters, $response);
+        return parent::renderWithExtraParams($view, $parameters, $response);
     }
 
     /**
