8ffd9280c8ed3b02f73a185a02ee77b3753bd834
Sonata CI <thomas+ci@sonata-project.org>
2018-09-10 04:15:13 +0200

    DevKit updates (#1471)

--- .travis/before_install_test.sh
+++ .travis/before_install_test.sh
@@ -19,20 +19,8 @@ fi
 
 sed --in-place "s/\"dev-master\":/\"dev-${TRAVIS_COMMIT}\":/" composer.json
 
-# TODO: remove when drop PHP 5 support
-# symfony/maker-bundle only works with PHP 7 and higher
-if [ "${TRAVIS_PHP_VERSION:0:3}" != "5.6" ]; then
-    # but only with Symfony 3.4 and higher
-    if [ "$SYMFONY" != "" ]; then
-        if [ "${SYMFONY:0:3}" != "2.8" ] && [ "${SYMFONY:0:3}" != "3.3" ] ; then
-            composer require "symfony/maker-bundle:$" --no-update
-        fi
-    else
-        composer require "symfony/maker-bundle:$" --no-update
-    fi
-fi
-
-if [ "$SYMFONY" != "" ]; then composer require "symfony/symfony:$SYMFONY" --no-update; fi;
-if [ "$DOCTRINE_ODM" != "" ]; then composer require "doctrine/mongodb-odm:$DOCTRINE_ODM" --no-update; fi;
-if [ "$SONATA_CORE" != "" ]; then composer require "sonata-project/core-bundle:$SONATA_CORE" --no-update; fi;
-if [ "$SONATA_ADMIN" != "" ]; then composer require "sonata-project/admin-bundle:$SONATA_ADMIN" --no-update; fi;
+            if [ "$SYMFONY" != "" ]; then composer require "symfony/symfony:$SYMFONY" --no-update; fi;
+                if [ "$DOCTRINE_ODM" != "" ]; then composer require "doctrine/mongodb-odm:$DOCTRINE_ODM" --no-update; fi;
+                if [ "$SONATA_CORE" != "" ]; then composer require "sonata-project/core-bundle:$SONATA_CORE" --no-update; fi;
+                if [ "$SONATA_ADMIN" != "" ]; then composer require "sonata-project/admin-bundle:$SONATA_ADMIN" --no-update; fi;
+    
