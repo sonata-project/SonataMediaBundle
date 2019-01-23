5dbf947f7a1ada52a0ae411779783fabb9200dfc
Sonata CI <thomas+ci@sonata-project.org>
2019-01-23 04:10:13 +0100

    DevKit updates (#1537)

--- .travis/before_install_test.sh
+++ .travis/before_install_test.sh
@@ -5,16 +5,12 @@ PHP_INI_DIR="$HOME/.phpenv/versions/$(phpenv version-name)/etc/conf.d/"
 TRAVIS_INI_FILE="$PHP_INI_DIR/travis.ini"
 echo "memory_limit=3072M" >> "$TRAVIS_INI_FILE"
 
-if [ "$TRAVIS_PHP_VERSION" '<' '7.0' ]; then
-    echo "extension=mongo.so" >> "$TRAVIS_INI_FILE"
-else
-    echo "extension=mongodb.so" >> "$TRAVIS_INI_FILE"
+echo "extension=mongodb.so" >> "$TRAVIS_INI_FILE"
 
-    # https://github.com/composer/composer/issues/5030
-    composer config "platform.ext-mongo" "1.6.16"
-    # Backwards compatibility with old mongo extension
-    composer require "alcaeus/mongo-php-adapter"
-fi
+# https://github.com/composer/composer/issues/5030
+composer config "platform.ext-mongo" "1.6.16"
+# Backwards compatibility with old mongo extension
+composer require "alcaeus/mongo-php-adapter"
 
 sed --in-place "s/\"dev-master\":/\"dev-${TRAVIS_COMMIT}\":/" composer.json
 
