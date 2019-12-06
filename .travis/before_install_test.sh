8d3d14223b86f820722d43ec4a49b813fdacdf8f
SonataCI <thomas+ci@sonata-project.org>
2019-12-06 02:08:28 +0100

    DevKit updates

--- .travis/before_install_test.sh
+++ .travis/before_install_test.sh
@@ -14,9 +14,8 @@ composer require "alcaeus/mongo-php-adapter"
 
 sed --in-place "s/\"dev-master\":/\"dev-${TRAVIS_COMMIT}\":/" composer.json
 
-    if [ "$SYMFONY" != "" ]; then composer require "symfony/symfony:$SYMFONY" --no-update; fi;
-        if [ "$DOCTRINE_ODM" != "" ]; then composer require "doctrine/mongodb-odm:$DOCTRINE_ODM" --no-update; fi;
-        if [ "$SONATA_CORE" != "" ]; then composer require "sonata-project/core-bundle:$SONATA_CORE" --no-update; fi;
-        if [ "$SONATA_ADMIN" != "" ]; then composer require "sonata-project/admin-bundle:$SONATA_ADMIN" --no-update; fi;
-        if [ "$IMAGINE" != "" ]; then composer require "imagine/imagine:$IMAGINE" --no-update; fi;
-    
+if [ "$SYMFONY" != "" ]; then composer require "symfony/symfony:$SYMFONY" --no-update; fi;
+if [ "$DOCTRINE_ODM" != "" ]; then composer require "doctrine/mongodb-odm:$DOCTRINE_ODM" --no-update; fi;
+if [ "$SONATA_CORE" != "" ]; then composer require "sonata-project/core-bundle:$SONATA_CORE" --no-update; fi;
+if [ "$SONATA_ADMIN" != "" ]; then composer require "sonata-project/admin-bundle:$SONATA_ADMIN" --no-update; fi;
+if [ "$IMAGINE" != "" ]; then composer require "imagine/imagine:$IMAGINE" --no-update; fi;
