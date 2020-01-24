58e17b917a3dff8417608aa376c8cc75833a20d6
SonataCI <thomas+ci@sonata-project.org>
2020-01-24 02:06:43 +0100

    DevKit updates

--- .travis/before_install_test.sh
+++ .travis/before_install_test.sh
@@ -16,6 +16,5 @@ sed --in-place "s/\"dev-master\":/\"dev-${TRAVIS_COMMIT}\":/" composer.json
 
 if [ "$SYMFONY" != "" ]; then composer require "symfony/symfony:$SYMFONY" --no-update; fi;
 if [ "$DOCTRINE_ODM" != "" ]; then composer require "doctrine/mongodb-odm:$DOCTRINE_ODM" --no-update; fi;
-if [ "$SONATA_CORE" != "" ]; then composer require "sonata-project/core-bundle:$SONATA_CORE" --no-update; fi;
 if [ "$SONATA_ADMIN" != "" ]; then composer require "sonata-project/admin-bundle:$SONATA_ADMIN" --no-update; fi;
 if [ "$IMAGINE" != "" ]; then composer require "imagine/imagine:$IMAGINE" --no-update; fi;
