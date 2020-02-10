e2af7f1b1a62d2503f7314e101092778b71386dd
Jeroen de Kok <jeroen@aveq.nl>
2020-02-10 08:44:29 +0100

    Move bootstrap code to test class + class constants

--- tests/Functional/Api/MediaControllerTest.php
+++ tests/Functional/Api/MediaControllerTest.php
@@ -13,15 +13,55 @@ declare(strict_types=1);
 
 namespace Sonata\MediaBundle\Tests\Functional;
 
+use Doctrine\Bundle\DoctrineBundle\Command\CreateDatabaseDoctrineCommand;
+use Doctrine\Bundle\DoctrineBundle\Command\DropDatabaseDoctrineCommand;
+use Doctrine\Bundle\DoctrineBundle\Command\Proxy\CreateSchemaDoctrineCommand;
 use Sonata\MediaBundle\Tests\App\AppKernel;
 use Symfony\Bundle\FrameworkBundle\Client;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\ConsoleOutput;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 
 final class MediaControllerTest extends WebTestCase
 {
     public function testGetMediaAction(): void
     {
-        $client = new Client(new AppKernel());
+        $kernel = new AppKernel();
+        $kernel->boot();
+        $doctrine = $kernel->getContainer()->get('doctrine');
+        $application = new Application($kernel);
+        $application->setAutoExit(false);
+
+        $interfaces = class_implements(CreateDatabaseDoctrineCommand::class);
+        if (isset($interfaces[ContainerAwareInterface::class])) {
+            $application->add(new DropDatabaseDoctrineCommand());
+            $application->add(new CreateDatabaseDoctrineCommand());
+        } else {
+            $application->add(new DropDatabaseDoctrineCommand($doctrine));
+            $application->add(new CreateDatabaseDoctrineCommand($doctrine));
+        }
+
+        $application->run(new ArrayInput([
+            'command' => 'doctrine:database:drop',
+            '--force' => '1',
+        ]));
+
+        $application->run(new ArrayInput([
+            'command' => 'doctrine:database:create',
+        ]));
+
+        $command = new CreateSchemaDoctrineCommand();
+        $application->add($command);
+        $input = new ArrayInput([
+            'command' => 'doctrine:schema:create',
+        ]);
+        $input->setInteractive(false);
+
+        $command->run($input, new ConsoleOutput());
+
+        $client = new Client($kernel);
         $client->request('GET', '/api/media');
         $this->assertSame(200, $client->getResponse()->getStatusCode());
     }
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -11,23 +11,6 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
-use Doctrine\Bundle\DoctrineBundle\Command\CreateDatabaseDoctrineCommand;
-use Doctrine\Bundle\DoctrineBundle\Command\DropDatabaseDoctrineCommand;
-use Doctrine\Bundle\DoctrineBundle\Command\Proxy\CreateSchemaDoctrineCommand;
-use Sonata\MediaBundle\Tests\App\AppKernel;
-use Symfony\Bundle\FrameworkBundle\Console\Application;
-use Symfony\Component\Console\Input\ArrayInput;
-use Symfony\Component\Console\Output\ConsoleOutput;
-
-/*
- * This file is part of the Sonata Project package.
- *
- * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 /*
  * fix encoding issue while running text on different host with different locale configuration
  */
@@ -52,37 +35,3 @@ $files = array_filter([
 if ($files) {
     require_once current($files);
 }
-
-$kernel = new AppKernel();
-$kernel->boot();
-$doctrine = $kernel->getContainer()->get('doctrine');
-$application = new Application($kernel);
-$application->setAutoExit(false);
-
-$interfaces = class_implements('Doctrine\Bundle\DoctrineBundle\Command\CreateDatabaseDoctrineCommand');
-
-if (isset($interfaces['Symfony\Component\DependencyInjection\ContainerAwareInterface'])) {
-    $application->add(new DropDatabaseDoctrineCommand());
-    $application->add(new CreateDatabaseDoctrineCommand());
-} else {
-    $application->add(new DropDatabaseDoctrineCommand($doctrine));
-    $application->add(new CreateDatabaseDoctrineCommand($doctrine));
-}
-
-$application->run(new ArrayInput([
-    'command' => 'doctrine:database:drop',
-    '--force' => '1',
-]));
-
-$application->run(new ArrayInput([
-    'command' => 'doctrine:database:create',
-]));
-
-$command = new CreateSchemaDoctrineCommand();
-$application->add($command);
-$input = new ArrayInput([
-    'command' => 'doctrine:schema:create',
-]);
-$input->setInteractive(false);
-
-$command->run($input, new ConsoleOutput());
