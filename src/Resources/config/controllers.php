361e1c80ff866f8d7e0ba11c2ad3ecde579750ba
Fabien Lucas <flucas2@gmail.com>
2021-06-29 17:36:34 +0200

    Add missing service declaration

--- src/Resources/config/controllers.php
+++ src/Resources/config/controllers.php
@@ -12,7 +12,9 @@ declare(strict_types=1);
  */
 
 use Psr\Container\ContainerInterface;
+use Sonata\MediaBundle\Controller\GalleryAdminController;
 use Sonata\MediaBundle\Controller\GalleryController;
+use Sonata\MediaBundle\Controller\MediaAdminController;
 use Sonata\MediaBundle\Controller\MediaController;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ReferenceConfigurator;
@@ -36,5 +38,17 @@ return static function (ContainerConfigurator $containerConfigurator): void {
                 new ReferenceConfigurator('sonata.media.pool'),
             ])
             ->tag('container.service_subscriber')
-            ->call('setContainer', [new ReferenceConfigurator(ContainerInterface::class)]);
+            ->call('setContainer', [new ReferenceConfigurator(ContainerInterface::class)])
+
+        ->set(MediaAdminController::class)
+            ->public()
+            ->tag('container.service_subscriber')
+            ->call('setContainer', [new ReferenceConfigurator(ContainerInterface::class)])
+
+        ->set(GalleryAdminController::class)
+            ->public()
+            ->tag('container.service_subscriber')
+            ->call('setContainer', [new ReferenceConfigurator(ContainerInterface::class)])
+    ;
+
 };
