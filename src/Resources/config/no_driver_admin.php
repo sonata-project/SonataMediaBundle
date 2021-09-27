007300e14c0e6d0768dacdb1fd8a8d524201aaff
Wojciech Błoszyk <wbloszyk@gmail.com>
2021-09-27 17:44:53 +0200

    Fix

--- src/Resources/config/no_driver_admin.php
+++ src/Resources/config/no_driver_admin.php
@@ -10,9 +10,7 @@ declare(strict_types=1);
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-
-return static function (ContainerConfigurator $containerConfigurator): void {
-    $containerConfigurator->services();
-};
+/*
+ * This file will be loaded when SonataAdminBundle is enabled and db_driver has its default value,
+ * not corresponding to real persistence mechanism.
+ */
