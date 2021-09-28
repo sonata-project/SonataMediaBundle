c07e2fb373ffc1c7f26e9407d52a2f6eb570f307
Wojciech Błoszyk <wbloszyk@gmail.com>
2021-09-28 08:55:24 +0200

    Add missing files (#2135)
    * Add missing files
    
    * Fix cs
    
    * Fix

---
+++ src/Resources/config/no_driver_admin.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Sonata Project package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+/*
+ * This file will be loaded when SonataAdminBundle is enabled and db_driver has its default value,
+ * not corresponding to real persistence mechanism.
+ */
