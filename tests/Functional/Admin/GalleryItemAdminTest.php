376fc677a578092646d3617e791fbd6018ea97f7
Jordi Sala Morales <jordism91@gmail.com>
2021-12-06 10:13:04 +0100

    Fix namespaces on tests

--- tests/Functional/Admin/GalleryItemAdminTest.php
+++ tests/Functional/Admin/GalleryItemAdminTest.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
-namespace Sonata\ClassificationBundle\Tests\App\Action;
+namespace Sonata\MediaBundle\Tests\Functional\Admin;
 
 use Doctrine\ORM\EntityManagerInterface;
 use Sonata\MediaBundle\Model\MediaInterface;
