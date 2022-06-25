9bf16b5268093078e780a664e3ab4d0831024330
Jordi Sala Morales <jordism91@gmail.com>
2022-06-25 09:08:25 +0200

    Fix deprecation while maintian lazyness

--- src/Command/FixMediaContextCommand.php
+++ src/Command/FixMediaContextCommand.php
@@ -16,12 +16,15 @@ namespace Sonata\MediaBundle\Command;
 use Sonata\ClassificationBundle\Model\CategoryManagerInterface;
 use Sonata\ClassificationBundle\Model\ContextManagerInterface;
 use Sonata\MediaBundle\Provider\Pool;
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
+#[AsCommand(name: 'sonata:media:fix-media-context', description: 'Generate the default category for each media context')]
 final class FixMediaContextCommand extends Command
 {
+    // TODO: Remove static properties when support for Symfony < 6.0 is dropped.
     protected static $defaultName = 'sonata:media:fix-media-context';
     protected static $defaultDescription = 'Generate the default category for each media context';
 
@@ -47,6 +50,7 @@ final class FixMediaContextCommand extends Command
     {
         \assert(null !== static::$defaultDescription);
 
+        // TODO: Remove setDescription when support for Symfony < 5.4 is dropped.
         $this->setDescription(static::$defaultDescription);
     }
 
