64742fdd8e10c4bc59df03ab5708463863678c4d
Vincent Langlet <vincentlanglet@hotmail.fr>
2022-12-03 18:53:45 +0100

    More fixes

--- src/Command/RemoveThumbsCommand.php
+++ src/Command/RemoveThumbsCommand.php
@@ -19,7 +19,6 @@ use Sonata\MediaBundle\Provider\MediaProviderInterface;
 use Sonata\MediaBundle\Provider\Pool;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Helper\QuestionHelper;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
