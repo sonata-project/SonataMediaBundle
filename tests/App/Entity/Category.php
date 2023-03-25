2cc28e0f68886df0147397c0c5eacc85805db526
Jordi Sala Morales <jordism91@gmail.com>
2023-03-25 17:26:02 +0100

    Actualizar Category.php

--- tests/App/Entity/Category.php
+++ tests/App/Entity/Category.php
@@ -26,11 +26,6 @@ class Category extends BaseCategory
     #[ORM\GeneratedValue]
     protected ?int $id = null;
 
-    public function setId(?int $id): void
-    {
-        $this->id = $id;
-    }
-
     public function getId()
     {
         return $this->id;
