4c0172b7df0d4b82404ecbb8ab5ae80112bb96c2
Vincent Langlet <vincentlanglet@hotmail.fr>
2024-06-19 12:40:00 +0200

    Solve usage of internal class

--- src/DependencyInjection/SonataMediaExtension.php
+++ src/DependencyInjection/SonataMediaExtension.php
@@ -22,10 +22,10 @@ use Sonata\Doctrine\Mapper\DoctrineCollector;
 use Symfony\Component\Config\Definition\Processor;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
 use Symfony\Component\DependencyInjection\Reference;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\Messenger\MessageBusInterface;
 
 /**
